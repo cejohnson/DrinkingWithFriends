3f9e5df6f52e4ab98f804090a23f8c70ac3243de
Chris Johnson <cjohnson@foodlogiq.com>
2013-11-17 14:13:42 -0500

    Initial commit, with bars, specials, and users, but no fields for any of the models

---
+++ app/models/special.rb
@@ -0,0 +1,3 @@
+class Special
+  include Mongoid::Document
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DrinkingWithFriends::Application.config.secret_token = '09dac0f07efeaedd6c80957c29086b46a80b18545f1837b40984d58d94f2290c2fda0397361dbe3ddb1e066e2f38f8a510f1df746fe2c154b30a8b650338962a'
